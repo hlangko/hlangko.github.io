1a6994977d56b2ddc8ae130fe0039224c8976733
hlangko <hee__song@naver.com>
2019-01-07 15:57:48 +0900

    feat: upload weather.js

---
+++ vanila/bg.js
@@ -0,0 +1,21 @@
+const body = document.querySelector('body');
+
+const IMG_NUMBER = 3;
+
+function printImage(imgNumber) {
+    const image = new Image();
+    image.src = `images/${imgNumber + 1}.jpg`;
+    body.appendChild(image);
+    image.classList.add('bgImage')
+}
+
+function getRandom() {
+    const number = Math.floor(Math.random() * IMG_NUMBER);
+    return number;
+}
+
+function init() {
+    const randomNumber = getRandom();
+    printImage(randomNumber);
+}
+init();
---
+++ vanila/weather.js
@@ -0,0 +1,31 @@
+const COORDS = 'coords';
+
+function saveCoords(coordsObj) {
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position) {
+    console.log(position.coords.latitude);
+}
+
+function handleGeoError() {
+    console.log('Cant access geo location');
+}
+
+function askForCoords() {
+    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords() {
+    const loadedCords = localStorage.getItem(COORDS);
+    if (loadedCords === null) {
+        askForCoords();
+    } else {
+        //get weather
+    }
+}
+
+function init() {
+    loadCoords();
+}
+init();
